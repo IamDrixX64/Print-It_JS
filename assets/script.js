4234cd158d5492ded1e6f43ddd86cd9a9a1b442c
Drixx Noname <drixxnoname@gmail.com>
2023-04-07 10:37:40 +0200

    add bullet slides

--- assets/script.js
+++ assets/script.js
@@ -16,16 +16,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+const arrows = document.querySelectorAll('.arrow')
+const arrowRight = document.querySelector('.arrow_right')
+const arrowLeft = document.querySelector('.arrow_left')
 
-const leftArrow = document.querySelector('.arrow_left')
-const rightArrow = document.querySelector('.arrow_right')
+arrows.forEach(arrow => arrow.addEventListener('click', function() {
+	this.classList.contains('arrow_left') ? console.log('le click gauche est utlisé') : console.log('le click droit est utilisé')
+}))
 
-leftArrow.addEventListener('click', function() {
-	alert('le click gauche est utilisé')
-})
-rightArrow.addEventListener('click', function() {
-	alert('le click gauche est utilisé')
-})
-rightArrow.addEventListener('contextmenu', function() {
-	alert('le click droit est utilisé')
-})
+for (var i = 0; i < slides.length; i++)
+    {
+        var li = document.createElement('li');
+		li.className = 'dot';
+		if ( i === 0) {
+			li.classList.add('dot_selected');
+		}
+		document.getElementById('mydot').appendChild(li);
+    }
+
+position = 0;
+arrowRight.addEventListener('click', function(){
+	if (position < slides.length - 1) {
+		position++;
+	} else {
+		position = 0
+	}
+	console.log(slides[position])
+	})
+arrowLeft.addEventListener('click', function(){
+	if (position > 0) {
+		position--;
+	} else {
+		position = slides.length - 1
+	}
+	console.log(slides[position])
+	})
